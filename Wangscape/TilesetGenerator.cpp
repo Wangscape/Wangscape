c208d811126db0cdbe711e2c6b70fded92253507
Serin Delaunay <serin.delaunay@gmail.com>
2016-12-08 04:53:59 +0000

    generateClique is not responsible for resolution

--- Wangscape/TilesetGenerator.cpp
+++ Wangscape/TilesetGenerator.cpp
@@ -26,6 +26,14 @@ void TilesetGenerator::generate(const Options & options)
     //}
     for (const auto& clique : options.cliques)
     {
+        size_t res_x = options.resolution;
+        size_t res_y = options.resolution;
+        bool x_dimension = true;
+        for (int i = 0; i < CORNERS; i++)
+        {
+            size_t& res_z = x_dimension ? res_x : res_y;
+            res_z *= clique.size();
+        }
         // prepare a blank image of size res_x*res_y
         generateClique(options, clique, nullptr, TileGenerator::generate);
         // write tileset image to disk
@@ -38,17 +46,8 @@ void TilesetGenerator::generateClique(const Options& options, const Options::Cli
     std::vector<Options::TerrainID> corner_terrains(CORNERS, clique[0]);
     std::vector<size_t> corner_clique_indices(CORNERS, 0);
     bool stop = false;
-    size_t res_x = options.resolution;
-    size_t res_y = options.resolution;
-    bool x_dimension = true;
-    for (int i = 0; i < CORNERS; i++)
-    {
-        size_t& res_z = x_dimension ? res_x : res_y;
-        res_z *= clique.size();
-    }
     while (!stop)
     {
-
         size_t x = 0; size_t y = 0;
         bool x_dimension = true;
         for (size_t i : corner_clique_indices)
