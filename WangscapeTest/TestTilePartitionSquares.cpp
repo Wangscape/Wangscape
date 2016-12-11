1a647448344e585089f177717c804ffa0bac97a8
Serin Delaunay <serin.delaunay@gmail.com>
2016-12-11 22:55:30 +0000

    Add TestSquarePartition

---
+++ WangscapeTest/TestTilePartitionSquares.cpp
@@ -0,0 +1,60 @@
+#include <gtest/gtest.h>
+#include <TilePartitionSquares.h>
+
+class TestTilePartitionSquares : public ::testing::Test {
+protected:
+    std::string filename;
+    const Options options;
+    TilePartition tp;
+
+    TestTilePartitionSquares() :
+        filename("../Wangscape/example/example_options.json"),
+        options(filename)
+    {
+        tile_partition_squares(tp, { "g","g","s","s" }, options);
+    };
+    ~TestTilePartitionSquares() {};
+};
+
+TEST_F(TestTilePartitionSquares, TestSquarePartition)
+{
+    sf::Image mask = tp[0].first.copyToImage();
+    EXPECT_EQ(mask.getPixel(0, 0), sf::Color(255, 255, 255, 255)) <<
+        "Mask 0: Wrong colour in top left pixel";
+    EXPECT_EQ(mask.getPixel(0, 31), sf::Color(255, 255, 255, 0)) <<
+        "Mask 0: Wrong colour in bottom left pixel";
+    EXPECT_EQ(mask.getPixel(31, 0), sf::Color(255, 255, 255, 0)) <<
+        "Mask 0: Wrong colour in top right pixel";
+    EXPECT_EQ(mask.getPixel(31, 31), sf::Color(255, 255, 255, 0)) <<
+        "Mask 0: Wrong colour in bottom right pixel";
+
+    mask = tp[1].first.copyToImage();
+    EXPECT_EQ(mask.getPixel(0, 0), sf::Color(255, 255, 255, 0)) <<
+        "Mask 1: Wrong colour in top left pixel";
+    EXPECT_EQ(mask.getPixel(0, 31), sf::Color(255, 255, 255, 255)) <<
+        "Mask 1: Wrong colour in bottom left pixel";
+    EXPECT_EQ(mask.getPixel(31, 0), sf::Color(255, 255, 255, 0)) <<
+        "Mask 1: Wrong colour in top right pixel";
+    EXPECT_EQ(mask.getPixel(31, 31), sf::Color(255, 255, 255, 0)) <<
+        "Mask 1: Wrong colour in bottom right pixel";
+
+    mask = tp[2].first.copyToImage();
+    EXPECT_EQ(mask.getPixel(0, 0), sf::Color(255, 255, 255, 0)) <<
+        "Mask 2: Wrong colour in top left pixel";
+    EXPECT_EQ(mask.getPixel(0, 31), sf::Color(255, 255, 255, 0)) <<
+        "Mask 2: Wrong colour in bottom left pixel";
+    EXPECT_EQ(mask.getPixel(31, 0), sf::Color(255, 255, 255, 255)) <<
+        "Mask 2: Wrong colour in top right pixel";
+    EXPECT_EQ(mask.getPixel(31, 31), sf::Color(255, 255, 255, 0)) <<
+        "Mask 2: Wrong colour in bottom right pixel";
+
+    mask = tp[3].first.copyToImage();
+    EXPECT_EQ(mask.getPixel(0, 0), sf::Color(255, 255, 255, 0)) <<
+        "Mask 3: Wrong colour in top left pixel";
+    EXPECT_EQ(mask.getPixel(0, 31), sf::Color(255, 255, 255, 0)) <<
+        "Mask 3: Wrong colour in bottom left pixel";
+    EXPECT_EQ(mask.getPixel(31, 0), sf::Color(255, 255, 255, 0)) <<
+        "Mask 3: Wrong colour in top right pixel";
+    EXPECT_EQ(mask.getPixel(31, 31), sf::Color(255, 255, 255, 255)) <<
+        "Mask 3: Wrong colour in bottom right pixel";
+}
