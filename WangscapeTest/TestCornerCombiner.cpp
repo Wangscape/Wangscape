b19d72b54d5f4271db6385c4feb37c7ce5322704
Serin Delaunay <serin.delaunay@gmail.com>
2016-12-12 19:15:39 +0000

    Test CornerCombinerGroup

--- WangscapeTest/TestCornerCombiner.cpp
+++ WangscapeTest/TestCornerCombiner.cpp
@@ -1,14 +1,16 @@
 #include <gtest/gtest.h>
 
 #include <CornerCombiner.h>
+#include <CornerCombinerGroup.h>
 
 class TestCornerCombiner : public ::testing::Test {
 protected:
     CornerCombiner cc1;
     CornerCombiner cc05;
     CornerCombiner cc2;
+    CornerCombinerGroup ccg2;
     TestCornerCombiner():
-        cc1(), cc05(0.5), cc2(2.)
+        cc1(), cc05(0.5), cc2(2.), ccg2(2.)
     {
 
     };
@@ -80,4 +82,24 @@ TEST_F(TestCornerCombiner, TestCornerCombiner2)
         "Incorrect value at (0,0.5) with power 2";
     EXPECT_NEAR(-1., cc2.GetValue(0., 1., 0.0001), 0.001) <<
         "Incorrect value at (0,1) with power 2";
+}
+
+TEST_F(TestCornerCombiner, TestCornerCombinerGroup2)
+{
+    EXPECT_NEAR(0.5, ccg2.getOutput().GetValue(0., 0., 0.0001), 0.00001) <<
+        "Incorrect clamped value at (0,0) with power 2";
+    EXPECT_NEAR(0.5, ccg2.getOutput().GetValue(0.5, 0.5, 0.0001), 0.0000) <<
+        "Incorrect clamped value at (0.5,0.5) with power 2";
+    EXPECT_NEAR(0.5, ccg2.getOutput().GetValue(1., 1., 0.0001), 0.0000) <<
+        "Incorrect clamped value at (1,1) with power 2";
+
+    EXPECT_EQ(1., ccg2.getOutput().GetValue(0.5, 0., 0.0001)) <<
+        "Incorrect clamped value at (0.5,0) with power 2";
+    EXPECT_NEAR(1., ccg2.getOutput().GetValue(1., 0., 0.0001), 0.001) <<
+        "Incorrect clamped value at (1,0) with power 2";
+
+    EXPECT_EQ(0., ccg2.getOutput().GetValue(0., 0.5, 0.0001)) <<
+        "Incorrect clamped value at (0,0.5) with power 2";
+    EXPECT_NEAR(0., ccg2.getOutput().GetValue(0., 1., 0.0001), 0.001) <<
+        "Incorrect clamped value at (0,1) with power 2";
 }
