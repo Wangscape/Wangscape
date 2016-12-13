f792107004835245040f74c1c33f3d3f8334fd43
Serin Delaunay <serin.delaunay@gmail.com>
2016-12-13 21:15:20 +0000

    Add corner stochastic masks to NoiseModuleManager
    And comment on how all user-defined masks are to be used.

--- Wangscape/NoiseModuleManager.h
+++ Wangscape/NoiseModuleManager.h
@@ -14,8 +14,23 @@ public:
     ~NoiseModuleManager();
     
 protected:
+    // User-defined masks specifying how two corner terrain types
+    // should blend in the region of horizontal border.
+    // Evaluated in the square [0,1]x[0,1] (if the corners are at the bottom)
+    // or [0,1]x[-1,0] (if the corners are at the top).
+    // Always evaluated with the same seed.
     std::map<TerrainIDPair, ModulePtr> mBordersHorizontal;
+    // User-defined masks specifying how two corner terrain types
+    // should blend in the region of a vertical border.
+    // Evaluated in the square [0,1]x[0,1] (if the corners are on the right)
+    // or [-1,0]x[0,1] (if the corners are on the left).
+    // Always evaluated with the same seed.
     std::map<TerrainIDPair, ModulePtr> mBordersVertical;
+    // User-defined masks specifying how a corner terrain type
+    // should blend with other corners in the region of the centre of the tile.
+    // Evaluated in the square [0,1]x[0,1].
+    // Normally evaluated with a different seed in every corner of every tile.
+    std::map<TerrainID, ModulePtr> mStochasticMasks;
 
 };
 
