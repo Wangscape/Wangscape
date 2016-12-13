fe7687999ed73c5c9c2ab73a5b694ff031bf6a28
Serin Delaunay <serin.delaunay@gmail.com>
2016-12-13 22:40:51 +0000

    ... and to the header file

--- Wangscape/ModuleFactories.h
+++ Wangscape/ModuleFactories.h
@@ -14,6 +14,7 @@ ModulePtr makeCornerCombiner(bool x_positive, bool y_positive, double power = 2.
 ModulePtr makeEdgeFavouringMask(double p, double q=1., double min=0.);
 
 // Makes a placeholder noise module (a Perlin noise module with typical parameters).
+ModulePtr makePlaceholder(int seed = 0,
                           int octaves = 5,
                           double frequency=1.,
                           double lacunarity=2.,
