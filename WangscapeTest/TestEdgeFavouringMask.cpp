29f8f7d6cd59fb641f06a8217fb5f6937e6cdf2f
Serin Delaunay <serin.delaunay@gmail.com>
2016-12-14 03:11:45 +0000

    Also update TestEdgeFavouringMask with Reseedables

--- WangscapeTest/TestEdgeFavouringMask.cpp
+++ WangscapeTest/TestEdgeFavouringMask.cpp
@@ -4,16 +4,16 @@
 
 class TestEdgeFavouringMask : public ::testing::Test {
 protected:
-    ModulePtr n1_1;
-    ModulePtr n1_2;
+    Reseedable n1_1;
+    Reseedable n1_2;
 
-    ModulePtr n2_1;
-    ModulePtr n2_2;
+    Reseedable n2_1;
+    Reseedable n2_2;
 
-    ModulePtr n05_1;
-    ModulePtr n05_05;
+    Reseedable n05_1;
+    Reseedable n05_05;
 
-    ModulePtr n2_1_05;
+    Reseedable n2_1_05;
 
     TestEdgeFavouringMask() :
         n1_1(makeEdgeFavouringMask(1)),
@@ -31,189 +31,189 @@ protected:
 
 TEST_F(TestEdgeFavouringMask, TestEdgeFavouringMask1_1)
 {
-    EXPECT_EQ(0., n1_1->GetValue(0.5, 0.5, 0.5)) <<
+    EXPECT_EQ(0., n1_1.first->GetValue(0.5, 0.5, 0.5)) <<
         "Incorrect value at origin";
 
-    EXPECT_EQ(1., n1_1->GetValue(1., 1., 0.)) <<
+    EXPECT_EQ(1., n1_1.first->GetValue(1., 1., 0.)) <<
         "Incorrect value at corner";
-    EXPECT_EQ(1., n1_1->GetValue(0., 1., 0.)) <<
+    EXPECT_EQ(1., n1_1.first->GetValue(0., 1., 0.)) <<
         "Incorrect value at corner";
-    EXPECT_EQ(1., n1_1->GetValue(1., 0., 0.)) <<
+    EXPECT_EQ(1., n1_1.first->GetValue(1., 0., 0.)) <<
         "Incorrect value at corner";
-    EXPECT_EQ(1., n1_1->GetValue(0., 0., 0.)) <<
+    EXPECT_EQ(1., n1_1.first->GetValue(0., 0., 0.)) <<
         "Incorrect value at corner";
 
-    EXPECT_EQ(1., n1_1->GetValue(1., 0.5, 0.)) <<
+    EXPECT_EQ(1., n1_1.first->GetValue(1., 0.5, 0.)) <<
         "Incorrect value on axis";
-    EXPECT_EQ(1., n1_1->GetValue(0., 0.5, 0.)) <<
+    EXPECT_EQ(1., n1_1.first->GetValue(0., 0.5, 0.)) <<
         "Incorrect value on axis";
-    EXPECT_EQ(1., n1_1->GetValue(0.5, 1., 0.)) <<
+    EXPECT_EQ(1., n1_1.first->GetValue(0.5, 1., 0.)) <<
         "Incorrect value on axis";
-    EXPECT_EQ(1., n1_1->GetValue(0.5, 0., 0.)) <<
+    EXPECT_EQ(1., n1_1.first->GetValue(0.5, 0., 0.)) <<
         "Incorrect value on axis";
 
-    EXPECT_EQ(1., n1_1->GetValue(0.75, 0.75, 0.)) <<
+    EXPECT_EQ(1., n1_1.first->GetValue(0.75, 0.75, 0.)) <<
         "Incorrect value";
 }
 
 TEST_F(TestEdgeFavouringMask, TestEdgeFavouringMask1_2)
 {
-    EXPECT_EQ(0., n1_2->GetValue(0.5, 0.5, 0.5)) <<
+    EXPECT_EQ(0., n1_2.first->GetValue(0.5, 0.5, 0.5)) <<
         "Incorrect value at origin";
 
-    EXPECT_EQ(1., n1_2->GetValue(1., 1., 0.)) <<
+    EXPECT_EQ(1., n1_2.first->GetValue(1., 1., 0.)) <<
         "Incorrect value at corner";
-    EXPECT_EQ(1., n1_2->GetValue(0., 1., 0.)) <<
+    EXPECT_EQ(1., n1_2.first->GetValue(0., 1., 0.)) <<
         "Incorrect value at corner";
-    EXPECT_EQ(1., n1_2->GetValue(1., 0., 0.)) <<
+    EXPECT_EQ(1., n1_2.first->GetValue(1., 0., 0.)) <<
         "Incorrect value at corner";
-    EXPECT_EQ(1., n1_2->GetValue(0., 0., 0.)) <<
+    EXPECT_EQ(1., n1_2.first->GetValue(0., 0., 0.)) <<
         "Incorrect value at corner";
 
-    EXPECT_EQ(1., n1_2->GetValue(1., 0.5, 0.)) <<
+    EXPECT_EQ(1., n1_2.first->GetValue(1., 0.5, 0.)) <<
         "Incorrect value on axis";
-    EXPECT_EQ(1., n1_2->GetValue(0., 0.5, 0.)) <<
+    EXPECT_EQ(1., n1_2.first->GetValue(0., 0.5, 0.)) <<
         "Incorrect value on axis";
-    EXPECT_EQ(1., n1_2->GetValue(0.5, 1., 0.)) <<
+    EXPECT_EQ(1., n1_2.first->GetValue(0.5, 1., 0.)) <<
         "Incorrect value on axis";
-    EXPECT_EQ(1., n1_2->GetValue(0.5, 0., 0.)) <<
+    EXPECT_EQ(1., n1_2.first->GetValue(0.5, 0., 0.)) <<
         "Incorrect value on axis";
 
-    EXPECT_NEAR(1., n1_2->GetValue(0.75, 0.75, 0.), 0.0001) <<
+    EXPECT_NEAR(1., n1_2.first->GetValue(0.75, 0.75, 0.), 0.0001) <<
         "Incorrect value";
 }
 
 TEST_F(TestEdgeFavouringMask, TestEdgeFavouringMask2_1)
 {
-    EXPECT_EQ(0., n2_1->GetValue(0.5, 0.5, 0.5)) <<
+    EXPECT_EQ(0., n2_1.first->GetValue(0.5, 0.5, 0.5)) <<
         "Incorrect value at origin";
 
-    EXPECT_EQ(1., n2_1->GetValue(1., 1., 0.)) <<
+    EXPECT_EQ(1., n2_1.first->GetValue(1., 1., 0.)) <<
         "Incorrect value at corner";
-    EXPECT_EQ(1., n2_1->GetValue(0., 1., 0.)) <<
+    EXPECT_EQ(1., n2_1.first->GetValue(0., 1., 0.)) <<
         "Incorrect value at corner";
-    EXPECT_EQ(1., n2_1->GetValue(1., 0., 0.)) <<
+    EXPECT_EQ(1., n2_1.first->GetValue(1., 0., 0.)) <<
         "Incorrect value at corner";
-    EXPECT_EQ(1., n2_1->GetValue(0., 0., 0.)) <<
+    EXPECT_EQ(1., n2_1.first->GetValue(0., 0., 0.)) <<
         "Incorrect value at corner";
 
-    EXPECT_EQ(1., n2_1->GetValue(1., 0.5, 0.)) <<
+    EXPECT_EQ(1., n2_1.first->GetValue(1., 0.5, 0.)) <<
         "Incorrect value on axis";
-    EXPECT_EQ(1., n2_1->GetValue(0., 0.5, 0.)) <<
+    EXPECT_EQ(1., n2_1.first->GetValue(0., 0.5, 0.)) <<
         "Incorrect value on axis";
-    EXPECT_EQ(1., n2_1->GetValue(0.5, 1., 0.)) <<
+    EXPECT_EQ(1., n2_1.first->GetValue(0.5, 1., 0.)) <<
         "Incorrect value on axis";
-    EXPECT_EQ(1., n2_1->GetValue(0.5, 0., 0.)) <<
+    EXPECT_EQ(1., n2_1.first->GetValue(0.5, 0., 0.)) <<
         "Incorrect value on axis";
 
-    EXPECT_NEAR(sqrt(0.5), n2_1->GetValue(0.75, 0.75, 0.), 0.0001) <<
+    EXPECT_NEAR(sqrt(0.5), n2_1.first->GetValue(0.75, 0.75, 0.), 0.0001) <<
         "Incorrect value";
 }
 
 TEST_F(TestEdgeFavouringMask, TestEdgeFavouringMask2_2)
 {
-    EXPECT_EQ(0., n2_2->GetValue(0.5, 0.5, 0.5)) <<
+    EXPECT_EQ(0., n2_2.first->GetValue(0.5, 0.5, 0.5)) <<
         "Incorrect value at origin";
 
-    EXPECT_EQ(1., n2_2->GetValue(1., 1., 0.)) <<
+    EXPECT_EQ(1., n2_2.first->GetValue(1., 1., 0.)) <<
         "Incorrect value at corner";
-    EXPECT_EQ(1., n2_2->GetValue(0., 1., 0.)) <<
+    EXPECT_EQ(1., n2_2.first->GetValue(0., 1., 0.)) <<
         "Incorrect value at corner";
-    EXPECT_EQ(1., n2_2->GetValue(1., 0., 0.)) <<
+    EXPECT_EQ(1., n2_2.first->GetValue(1., 0., 0.)) <<
         "Incorrect value at corner";
-    EXPECT_EQ(1., n2_2->GetValue(0., 0., 0.)) <<
+    EXPECT_EQ(1., n2_2.first->GetValue(0., 0., 0.)) <<
         "Incorrect value at corner";
 
-    EXPECT_EQ(1., n2_2->GetValue(1., 0.5, 0.)) <<
+    EXPECT_EQ(1., n2_2.first->GetValue(1., 0.5, 0.)) <<
         "Incorrect value on axis";
-    EXPECT_EQ(1., n2_2->GetValue(0., 0.5, 0.)) <<
+    EXPECT_EQ(1., n2_2.first->GetValue(0., 0.5, 0.)) <<
         "Incorrect value on axis";
-    EXPECT_EQ(1., n2_2->GetValue(0.5, 1., 0.)) <<
+    EXPECT_EQ(1., n2_2.first->GetValue(0.5, 1., 0.)) <<
         "Incorrect value on axis";
-    EXPECT_EQ(1., n2_2->GetValue(0.5, 0., 0.)) <<
+    EXPECT_EQ(1., n2_2.first->GetValue(0.5, 0., 0.)) <<
         "Incorrect value on axis";
 
-    EXPECT_NEAR(0.5, n2_2->GetValue(0.75, 0.75, 0.), 0.0001) <<
+    EXPECT_NEAR(0.5, n2_2.first->GetValue(0.75, 0.75, 0.), 0.0001) <<
         "Incorrect value";
 }
 
 TEST_F(TestEdgeFavouringMask, TestEdgeFavouringMask05_1)
 {
-    EXPECT_EQ(0., n05_1->GetValue(0.5, 0.5, 0.5)) <<
+    EXPECT_EQ(0., n05_1.first->GetValue(0.5, 0.5, 0.5)) <<
         "Incorrect value at origin";
 
-    EXPECT_EQ(1., n05_1->GetValue(1., 1., 0.)) <<
+    EXPECT_EQ(1., n05_1.first->GetValue(1., 1., 0.)) <<
         "Incorrect value at corner";
-    EXPECT_EQ(1., n05_1->GetValue(0., 1., 0.)) <<
+    EXPECT_EQ(1., n05_1.first->GetValue(0., 1., 0.)) <<
         "Incorrect value at corner";
-    EXPECT_EQ(1., n05_1->GetValue(1., 0., 0.)) <<
+    EXPECT_EQ(1., n05_1.first->GetValue(1., 0., 0.)) <<
         "Incorrect value at corner";
-    EXPECT_EQ(1., n05_1->GetValue(0., 0., 0.)) <<
+    EXPECT_EQ(1., n05_1.first->GetValue(0., 0., 0.)) <<
         "Incorrect value at corner";
 
-    EXPECT_EQ(1., n05_1->GetValue(1., 0.5, 0.)) <<
+    EXPECT_EQ(1., n05_1.first->GetValue(1., 0.5, 0.)) <<
         "Incorrect value on axis";
-    EXPECT_EQ(1., n05_1->GetValue(0., 0.5, 0.)) <<
+    EXPECT_EQ(1., n05_1.first->GetValue(0., 0.5, 0.)) <<
         "Incorrect value on axis";
-    EXPECT_EQ(1., n05_1->GetValue(0.5, 1., 0.)) <<
+    EXPECT_EQ(1., n05_1.first->GetValue(0.5, 1., 0.)) <<
         "Incorrect value on axis";
-    EXPECT_EQ(1., n05_1->GetValue(0.5, 0., 0.)) <<
+    EXPECT_EQ(1., n05_1.first->GetValue(0.5, 0., 0.)) <<
         "Incorrect value on axis";
 
-    EXPECT_EQ(1., n05_1->GetValue(0.75, 0.75, 0.)) <<
+    EXPECT_EQ(1., n05_1.first->GetValue(0.75, 0.75, 0.)) <<
         "Incorrect value";
 }
 
 TEST_F(TestEdgeFavouringMask, TestEdgeFavouringMask05_05)
 {
-    EXPECT_EQ(0., n05_05->GetValue(0.5, 0.5, 0.5)) <<
+    EXPECT_EQ(0., n05_05.first->GetValue(0.5, 0.5, 0.5)) <<
         "Incorrect value at origin";
 
-    EXPECT_EQ(1., n05_05->GetValue(1., 1., 0.)) <<
+    EXPECT_EQ(1., n05_05.first->GetValue(1., 1., 0.)) <<
         "Incorrect value at corner";
-    EXPECT_EQ(1., n05_05->GetValue(0., 1., 0.)) <<
+    EXPECT_EQ(1., n05_05.first->GetValue(0., 1., 0.)) <<
         "Incorrect value at corner";
-    EXPECT_EQ(1., n05_05->GetValue(1., 0., 0.)) <<
+    EXPECT_EQ(1., n05_05.first->GetValue(1., 0., 0.)) <<
         "Incorrect value at corner";
-    EXPECT_EQ(1., n05_05->GetValue(0., 0., 0.)) <<
+    EXPECT_EQ(1., n05_05.first->GetValue(0., 0., 0.)) <<
         "Incorrect value at corner";
 
-    EXPECT_EQ(1., n05_05->GetValue(1., 0.5, 0.)) <<
+    EXPECT_EQ(1., n05_05.first->GetValue(1., 0.5, 0.)) <<
         "Incorrect value on axis";
-    EXPECT_EQ(1., n05_05->GetValue(0., 0.5, 0.)) <<
+    EXPECT_EQ(1., n05_05.first->GetValue(0., 0.5, 0.)) <<
         "Incorrect value on axis";
-    EXPECT_EQ(1., n05_05->GetValue(0.5, 1., 0.)) <<
+    EXPECT_EQ(1., n05_05.first->GetValue(0.5, 1., 0.)) <<
         "Incorrect value on axis";
-    EXPECT_EQ(1., n05_05->GetValue(0.5, 0., 0.)) <<
+    EXPECT_EQ(1., n05_05.first->GetValue(0.5, 0., 0.)) <<
         "Incorrect value on axis";
 
-    EXPECT_EQ(1., n05_05->GetValue(0.75, 0.75, 0.)) <<
+    EXPECT_EQ(1., n05_05.first->GetValue(0.75, 0.75, 0.)) <<
         "Incorrect value";
 }
 
 TEST_F(TestEdgeFavouringMask, TestEdgeFavouringMask2_1_05)
 {
-    EXPECT_NEAR(0.5, n2_1_05->GetValue(0.5, 0.5, 0.5), 0.0001) <<
+    EXPECT_NEAR(0.5, n2_1_05.first->GetValue(0.5, 0.5, 0.5), 0.0001) <<
         "Incorrect value at origin";
 
-    EXPECT_EQ(1., n2_1_05->GetValue(1., 1., 0.)) <<
+    EXPECT_EQ(1., n2_1_05.first->GetValue(1., 1., 0.)) <<
         "Incorrect value at corner";
-    EXPECT_EQ(1., n2_1_05->GetValue(0., 1., 0.)) <<
+    EXPECT_EQ(1., n2_1_05.first->GetValue(0., 1., 0.)) <<
         "Incorrect value at corner";
-    EXPECT_EQ(1., n2_1_05->GetValue(1., 0., 0.)) <<
+    EXPECT_EQ(1., n2_1_05.first->GetValue(1., 0., 0.)) <<
         "Incorrect value at corner";
-    EXPECT_EQ(1., n2_1_05->GetValue(0., 0., 0.)) <<
+    EXPECT_EQ(1., n2_1_05.first->GetValue(0., 0., 0.)) <<
         "Incorrect value at corner";
 
-    EXPECT_EQ(1., n2_1_05->GetValue(1., 0.5, 0.)) <<
+    EXPECT_EQ(1., n2_1_05.first->GetValue(1., 0.5, 0.)) <<
         "Incorrect value on axis";
-    EXPECT_EQ(1., n2_1_05->GetValue(0., 0.5, 0.)) <<
+    EXPECT_EQ(1., n2_1_05.first->GetValue(0., 0.5, 0.)) <<
         "Incorrect value on axis";
-    EXPECT_EQ(1., n2_1_05->GetValue(0.5, 1., 0.)) <<
+    EXPECT_EQ(1., n2_1_05.first->GetValue(0.5, 1., 0.)) <<
         "Incorrect value on axis";
-    EXPECT_EQ(1., n2_1_05->GetValue(0.5, 0., 0.)) <<
+    EXPECT_EQ(1., n2_1_05.first->GetValue(0.5, 0., 0.)) <<
         "Incorrect value on axis";
 
-    EXPECT_NEAR(0.5 + sqrt(0.125), n2_1_05->GetValue(0.75, 0.75, 0.), 0.0001) <<
+    EXPECT_NEAR(0.5 + sqrt(0.125), n2_1_05.first->GetValue(0.75, 0.75, 0.), 0.0001) <<
         "Incorrect value";
 }
