cb52ff6b021f616f94e9208ef1e300d504e7a208
Serin Delaunay <serin.delaunay@gmail.com>
2016-12-14 02:08:52 +0000

    Factor out the key problem
    And correct lambda signature

--- Wangscape/Reseedable.h
+++ Wangscape/Reseedable.h
@@ -11,15 +11,21 @@ typedef std::function<void(int)> SetSeedFn;
 typedef std::pair<ModulePtr, SetSeedFn> Reseedable;
 
 template<typename T>
+SetSeedFn getSetSeed(std::shared_ptr<T> module_ptr)
+{
+    // TODO return module_ptr->SetSeed if that function exists
+    return [](int seed) {};
+};
+
+template<typename T>
 Reseedable makeReseedable(std::shared_ptr<T> module_ptr)
 {
-    // TODO make set_seed equal to module_ptr->SetSeed if that function exists
-    SetSeedFn set_seed = []() {};
-    return{ (ModulePtr)module_ptr, set_seed };
+    return{ (ModulePtr)module_ptr, getSetSeed<T>(module_ptr) };
 };
 
 template<typename T>
 Reseedable makeNonReseedable(std::shared_ptr<T> module_ptr)
 {
-    return{ (ModulePtr)module_ptr, []() {} };
+    return{ (ModulePtr)module_ptr, [](int seed) {} };
 };
+
