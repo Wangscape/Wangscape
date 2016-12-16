86f0896ec87742dbf2b418bcfb640dba93e4df2b
Serin Delaunay <serin.delaunay@gmail.com>
2016-12-16 14:20:44 +0000

    Split NoiseMap into vector and image versions

--- Wangscape/NoiseMap.cpp
+++ Wangscape/NoiseMap.cpp
@@ -1,64 +1,125 @@
 #include "NoiseMap.h"
 
-NoiseMap::NoiseMap(size_t x, size_t y):
-    mSizeX(x), mSizeY(y), mData(x*y)
+NoiseMapBase::NoiseMapBase()
 {
 }
 
-NoiseMap::NoiseMap(size_t x, size_t y, NoiseMap::Bounds bounds):
-    mSizeX(x), mSizeY(y), mBounds(bounds)
+NoiseMapBase::NoiseMapBase(Bounds bounds)
 {
 }
 
-NoiseMap::NoiseMap(size_t x, size_t y, NoiseMap::Bounds bounds, const NoiseModule & module):
-    mSizeX(x), mSizeY(y), mBounds(bounds)
-{
-    build(module);
-}
-
-size_t NoiseMap::getX() const
-{
-    return mSizeX;
-}
-
-size_t NoiseMap::getY() const
-{
-    return mSizeY;
-}
-
-const NoiseMap::Bounds & NoiseMap::bounds() const
+const NoiseMapBase::Bounds & NoiseMapBase::bounds() const
 {
     return mBounds;
 }
 
-NoiseMap::Bounds & NoiseMap::bounds()
+NoiseMapBase::Bounds & NoiseMapBase::bounds()
 {
     return mBounds;
 }
 
-void NoiseMap::build(const NoiseModule & module)
+void NoiseMapBase::build(const NoiseModule & module)
 {
-    Real step_x = bounds().width / getX();
-    Real step_y = bounds().height / getY();
+    Real step_x = bounds().width / sizeX();
+    Real step_y = bounds().height / sizeY();
     Real start_x = bounds().left + 0.5*step_x;
     Real start_y = bounds().top + 0.5*step_y;
-    for (size_t x = 0; x < getX(); x++)
+    for (size_t x = 0; x < sizeX(); x++)
     {
-        for (size_t y = 0; y < getY(); y++)
+        for (size_t y = 0; y < sizeY(); y++)
         {
-            mData[index(x,y)] = module.GetValue(start_x + x*step_x, start_y + y*step_y, 0.0);
+            set(x, y, module.GetValue(start_x + x*step_x,
+                                      start_y + y*step_y,
+                                      0.0));
         }
     }
 }
 
-NoiseMap::Real NoiseMap::get(size_t x, size_t y) const
+NoiseMapVector::NoiseMapVector(size_t x, size_t y) :
+    mSizeX(x), mSizeY(y), mData(x*y)
+{
+}
+
+NoiseMapVector::NoiseMapVector(size_t x, size_t y, NoiseMapBase::Bounds bounds) :
+    NoiseMapBase(bounds),
+    mSizeX(x), mSizeY(y)
+{
+}
+
+NoiseMapVector::NoiseMapVector(size_t x, size_t y, NoiseMapBase::Bounds bounds, const NoiseModule & module) :
+    NoiseMapBase(bounds),
+    mSizeX(x), mSizeY(y)
+{
+    build(module);
+}
+
+size_t NoiseMapVector::sizeX() const
+{
+    return mSizeX;
+}
+
+size_t NoiseMapVector::sizeY() const
+{
+    return mSizeY;
+}
+
+NoiseMapBase::Real NoiseMapVector::get(size_t x, size_t y) const
 {
     return mData[index(x, y)];
 }
 
-size_t NoiseMap::index(size_t x, size_t y) const
+void NoiseMapVector::set(size_t x, size_t y, Real value)
+{
+    mData[index(x, y)] = value;
+}
+
+size_t NoiseMapVector::index(size_t x, size_t y) const
 {
     if (x >= mSizeX || y >= mSizeY)
         throw std::out_of_range("Tried to access NoiseMap with x or y out of bounds");
     return y*mSizeX + x;
+}
+
+NoiseMapImage::NoiseMapImage(Image & image) :
+    mImage(image)
+{
+}
+
+NoiseMapImage::NoiseMapImage(Image & image, Bounds bounds) :
+    mImage(image),
+    NoiseMapBase(bounds)
+{
+}
+
+NoiseMapImage::NoiseMapImage(Image & image, Bounds bounds, const NoiseModule & module) :
+    mImage(image),
+    NoiseMapBase(bounds)
+{
+    build(module);
+}
+
+size_t NoiseMapImage::sizeX() const
+{
+    return mImage.getSize().x;
+}
+
+size_t NoiseMapImage::sizeY() const
+{
+    return mImage.getSize().y;
+}
+
+NoiseMapImage::Colour NoiseMapImage::get(size_t x, size_t y) const
+{
+    return mImage.getPixel(x, y);
+}
+
+NoiseMapImage::Colour NoiseMapImage::makeColour(Real value)
+{
+    sf::Uint8 v = int((value + 1)*255. / 2.);
+    return Colour(v, v, v, 255);
+}
+
+void NoiseMapImage::set(size_t x, size_t y, Real value)
+{
+    mImage.setPixel(x, y, makeColour(value));
 }
--- Wangscape/NoiseMap.h
+++ Wangscape/NoiseMap.h
@@ -1,27 +1,64 @@
 #include <vector>
 #include <noise/noise.h>
-#include <SFML/Graphics/Rect.hpp>
+#include <SFML/Graphics.hpp>
+#include <functional>
 
-class NoiseMap
+class NoiseMapBase
 {
 public:
     typedef noise::module::Module NoiseModule;
     typedef double Real;
     typedef sf::Rect<Real> Bounds;
-    typedef std::vector<Real> Data;
-    NoiseMap(size_t x, size_t y);
-    NoiseMap(size_t x, size_t y, Bounds bounds);
-    NoiseMap(size_t x, size_t y, Bounds bounds, const NoiseModule& module);
-    size_t getX() const;
-    size_t getY() const;
+
+    NoiseMapBase();
+    NoiseMapBase(Bounds bounds);
+
     const Bounds& bounds() const;
     Bounds& bounds();
     void build(const NoiseModule& module);
+
+    virtual size_t sizeX() const = 0;
+    virtual size_t sizeY() const = 0;
+    virtual void set(size_t x, size_t y, Real value) const = 0;
+protected:
+    virtual void set(size_t x, size_t y, Real value) = 0;
+    Bounds mBounds;
+};
+
+class NoiseMapVector : public NoiseMapBase
+{
+public:
+    typedef std::vector<Real> Data;
+    NoiseMapVector(size_t x, size_t y);
+    NoiseMapVector(size_t x, size_t y, Bounds bounds);
+    NoiseMapVector(size_t x, size_t y, Bounds bounds, const NoiseModule& module);
+
+    virtual size_t sizeX() const;
+    virtual size_t sizeY() const;
     Real get(size_t x, size_t y) const;
 protected:
+    void set(size_t x, size_t y, Real value);
     size_t index(size_t x, size_t y) const;
     size_t mSizeX;
     size_t mSizeY;
-    Bounds mBounds;
     Data mData;
+};
+
+class NoiseMapImage : public NoiseMapBase
+{
+    typedef sf::Color Colour;
+    typedef std::function<Colour(Real)> ColourMaker;
+    typedef sf::Image Image;
+
+    NoiseMapImage(Image& image);
+    NoiseMapImage(Image& image, Bounds bounds);
+    NoiseMapImage(Image& image, Bounds bounds, const NoiseModule& module);
+
+    virtual size_t sizeX() const;
+    virtual size_t sizeY() const;
+    Colour get(size_t x, size_t y) const;
+    virtual Colour makeColour(Real value);
+protected:
+    virtual void set(size_t x, size_t y, Real value);
+    sf::Image& mImage;
 };
