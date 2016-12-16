daca99c2fa0e10bd20dd526081e5304776320ede
Serin Delaunay <serin.delaunay@gmail.com>
2016-12-16 05:07:19 +0000

    Correct module factory comments

--- Wangscape/ModuleFactories.h
+++ Wangscape/ModuleFactories.h
@@ -8,10 +8,10 @@ Reseedable makePeak(bool x);
 
 //ModuleGroup::ModulePtr makeQuadrantSelector(bool x,  bool y)
 
-// Makes a CornerCombiner scaled and clamped to the interval [0., 1.].
+// Makes a CornerCombiner scaled and clamped to the interval [-1., 1.].
 Reseedable makeCornerCombiner(bool x_positive, bool y_positive, double power = 2.);
 
-// Makes a NormLPQ clamped to the interval [0., 1.].
+// Makes a NormLPQ clamped to the interval [min, 1.].
 Reseedable makeEdgeFavouringMask(double p, double q=1., double min=-1.);
 
 // Makes a placeholder noise module (a Perlin noise module with typical parameters).
