4fe0baec9b291c53eceb35c25b87a4b18d9249f2
Serin Delaunay <serin.delaunay@gmail.com>
2016-12-18 13:42:42 +0000

    Initialise and retrieve modules from NoiseModuleManager

--- Wangscape/NoiseModuleManager.cpp
+++ Wangscape/NoiseModuleManager.cpp
@@ -1,12 +1,46 @@
 #include "NoiseModuleManager.h"
+#include "ModuleFactories.h"
+#include <random>
+#include <time.h>
 
-
-
-NoiseModuleManager::NoiseModuleManager()
+NoiseModuleManager::NoiseModuleManager(const Options & options)
 {
+    for (const auto& terrain : options.terrains)
+    {
+        mStochasticMasks.insert({ terrain.first, makePlaceholder() });
+    }
+    std::mt19937 rng;
+    rng.seed(time(nullptr));
+    for (const auto& clique : options.cliques)
+    {
+        // At least n*(n-1)/2 duplicates will be checked in each clique.
+        for (const auto& t1 : clique)
+            for (const auto& t2 : clique)
+            {
+                TerrainIDPair tp{ t1,t2 };
+                if (mBordersHorizontal.find(tp) == mBordersHorizontal.end())
+                    mBordersHorizontal.insert({ tp,makePlaceholder(rng()) });
+                if (mBordersVertical.find(tp) == mBordersVertical.end())
+                    mBordersVertical.insert({ tp,makePlaceholder(rng()) });
+            }
+    }
 }
-
 
 NoiseModuleManager::~NoiseModuleManager()
 {
+}
+
+const Reseedable NoiseModuleManager::getBorderVertical(TerrainID top, TerrainID bottom) const
+{
+    return mBordersVertical.at({ top,bottom });
+}
+
+const Reseedable NoiseModuleManager::getBorderHorizontal(TerrainID left, TerrainID right) const
+{
+    return mBordersHorizontal.at({ left,right });
+}
+
+Reseedable NoiseModuleManager::getStochastic(TerrainID terrain)
+{
+    return mStochasticMasks.at(terrain);
 }
--- Wangscape/NoiseModuleManager.h
+++ Wangscape/NoiseModuleManager.h
@@ -5,13 +5,18 @@
 #include "common.h"
 #include "Gradient.h"
 #include "Reseedable.h"
+#include "Options.h"
 class NoiseModuleManager
 {
 public:
     typedef noise::module::Module Module;
     typedef std::pair<TerrainID, TerrainID> TerrainIDPair;
-    NoiseModuleManager();
+    NoiseModuleManager(const Options& options);
     ~NoiseModuleManager();
+
+    const Reseedable getBorderVertical(TerrainID top, TerrainID bottom) const;
+    const Reseedable getBorderHorizontal(TerrainID left, TerrainID right) const;
+    Reseedable getStochastic(TerrainID terrain);
     
 protected:
     // User-defined masks specifying how two corner terrain types
@@ -20,12 +25,14 @@ protected:
     // or [0,1]x[-1,0] (if the corners are at the top).
     // Reseeding will make terrain borders incompatible.
     std::map<TerrainIDPair, Reseedable> mBordersHorizontal;
+
     // User-defined masks specifying how two corner terrain types
     // should blend in the region of a vertical border.
     // Evaluated in the square [0,1]x[0,1] (if the corners are on the right)
     // or [-1,0]x[0,1] (if the corners are on the left).
     // Reseeding will make terrain borders incompatible.
     std::map<TerrainIDPair, Reseedable> mBordersVertical;
+
     // User-defined masks specifying how a corner terrain type
     // should blend with other corners in the region of the centre of the tile.
     // Evaluated in the square [0,1]x[0,1].
