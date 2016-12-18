f8c878137493f787fab1b29880fbcf1434e2a8f8
Serin Delaunay <serin.delaunay@gmail.com>
2016-12-18 17:17:43 +0000

    Update TestTilePartitionerGradient

--- WangscapeTest/TestTilePartitionerGradient.cpp
+++ WangscapeTest/TestTilePartitionerGradient.cpp
@@ -1,35 +1,38 @@
 #include <gtest/gtest.h>
 
-#include <TilePartitionGradient.h>
+#include <TilePartitionerGradient.h>
 #include <Options.h>
 
-class TestTilePartitionGradient: public ::testing::Test{
+class TestTilePartitionerGradient : public ::testing::Test{
 protected:
     std::string filename;
     const Options options;
-    TilePartition tp;
+    TilePartitionerGradient tpg;
 
-    TestTilePartitionGradient() :
+
+    TestTilePartitionerGradient() :
         filename("../Wangscape/example/example_options.json"),
-        options(filename)
+        options(filename),
+        tpg(options)
     {
-        tile_partition_gradient(tp,{ "g","g","s","s" }, options);
     };
-    ~TestTilePartitionGradient() {};
+    ~TestTilePartitionerGradient() {};
 };
 
  
-TEST_F(TestTilePartitionGradient, TestGradientWeight)
+TEST_F(TestTilePartitionerGradient, TestGradientWeight)
 {
     // Doesn't use the test fixture - move to a different test case?
-    EXPECT_EQ(31, gradient_weight(0, 0, 0, 0, 31));
-    EXPECT_EQ(0, gradient_weight(0, 0, 31, 0, 31));
-    EXPECT_EQ(0, gradient_weight(0, 0, 0, 31, 31));
-    EXPECT_EQ(0, gradient_weight(0, 0, 31, 31, 31));
+    EXPECT_EQ(31, tpg.gradientWeight(0, 0, 0, 0, 31));
+    EXPECT_EQ(0, tpg.gradientWeight(0, 0, 31, 0, 31));
+    EXPECT_EQ(0, tpg.gradientWeight(0, 0, 0, 31, 31));
+    EXPECT_EQ(0, tpg.gradientWeight(0, 0, 31, 31, 31));
 }
 
-TEST_F(TestTilePartitionGradient, TestGradientPartition)
+TEST_F(TestTilePartitionerGradient, TestGradientPartition)
 {
+    TilePartitionerGradient::TilePartition tp;
+    tpg.makePartition(tp, { "g","g","s","s" });
     sf::Image mask = tp[0].first.copyToImage();
     EXPECT_EQ(mask.getPixel(0, 0), sf::Color(255, 255, 255, 255)) <<
         "Mask 0: Wrong colour in top left pixel";
