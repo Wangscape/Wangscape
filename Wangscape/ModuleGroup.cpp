4b600935fc125e9a7a4d4c5c2e560615d005de37
Serin Delaunay <serin.delaunay@gmail.com>
2016-12-26 17:25:34 +0000

    Virtual default destructor  for ModuleGroup

--- Wangscape/ModuleGroup.cpp
+++ Wangscape/ModuleGroup.cpp
@@ -33,11 +33,6 @@ int ModuleGroup::GetSourceModuleCount() const
     return 0;
 }
 
-ModuleGroup::~ModuleGroup()
-{
-
-};
-
 double ModuleGroup::GetValue(double x, double y, double z) const
 {
     return mModules.at(output_id).module.get()->GetValue(x, y, z);
