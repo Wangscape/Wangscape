6809a61416ede3f1baac8c7c6bd76055c33eac1f
Łukasz Hryniuk <lukasz.hryniuk@wp.pl>
2016-12-26 14:30:01 +0100

    Add constructors to Tileset

---
+++ Wangscape/Tileset.cpp
@@ -0,0 +1,7 @@
+#include "Tileset.h"
+
+
+Tileset::Tileset(std::string filename_, size_t resolution_, size_t x_, size_t y_, std::vector<TerrainID> terrains_)
+    : filename(filename_), resolution(resolution_), x(x_), y(y_), terrains(terrains_)
+{
+}
--- Wangscape/Tileset.h
+++ Wangscape/Tileset.h
@@ -7,6 +7,8 @@
 
 struct Tileset
 {
+    Tileset() = default;
+    Tileset(std::string filename_, size_t resolution_, size_t x_, size_t y_, std::vector<TerrainID> terrains_);
     std::string filename;
     size_t resolution;
     size_t x;
