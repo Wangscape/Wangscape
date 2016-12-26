416ae0a03c4b933bed1a51644d5301c3d906fb80
Łukasz Hryniuk <lukasz.hryniuk@wp.pl>
2016-12-27 00:48:34 +0100

    Add Tile struct

---
+++ Wangscape/Tile.cpp
@@ -0,0 +1,6 @@
+#include "Tile.h"
+
+Tile::Tile(std::vector<std::string> corners_, std::string filename_, size_t x_, size_t y_)
+    : corners(corners_), filename(filename_), x(x_), y(y_)
+{
+}
---
+++ Wangscape/Tile.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+struct Tile
+{
+    Tile() = default;
+    Tile(std::vector<std::string> corners_, std::string filename_, size_t x_, size_t y_);
+    std::vector<std::string> corners;
+    std::string filename;
+    size_t x;
+    size_t y;
+};
