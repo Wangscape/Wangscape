25d896ce2c54bb9ea037208b2fc09073414f223d
Łukasz Hryniuk <lukasz.hryniuk@wp.pl>
2016-12-27 18:41:39 +0100

    Add OutputFilenames struct

---
+++ Wangscape/OutputFilenames.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+
+struct OutputFilenames
+{
+    std::string tileDataFilename;
+    std::string tileGroupsFilename;
+    std::string tilesetDataFilename;
+    std::string terrainHypergraphFilename;
+};
