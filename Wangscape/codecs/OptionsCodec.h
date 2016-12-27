04a544973f5e30285ddc6726c08084ee24b7b82c
Łukasz Hryniuk <lukasz.hryniuk@wp.pl>
2016-12-27 19:46:53 +0100

    Add Options codec

---
+++ Wangscape/codecs/OptionsCodec.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <spotify/json.hpp>
+
+#include "Options.h"
+#include "OutputFilenamesCodec.h"
+#include "TerrainSpecCodec.h"
+#include "TileFormatCodec.h"
+
+using namespace spotify::json::codec;
+
+namespace spotify
+{
+namespace json
+{
+
+template<>
+struct default_codec_t<Options>
+{
+    static object_t<Options> codec()
+    {
+        auto codec = object<Options>();
+        codec.required("OutputDirectory", &Options::outputDirectory);
+        codec.required("Terrains", &Options::terrains);
+        codec.required("TileFormat", &Options::tileFormat);
+        codec.required("Cliques", &Options::cliques);
+        codec.required("MetaOutput", &Options::outputFilenames);
+
+        return codec;
+    }
+};
+
+}
+}
