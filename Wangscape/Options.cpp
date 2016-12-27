513f80cb1d533057bbc2d1eb232d13a100a2459c
Łukasz Hryniuk <lukasz.hryniuk@wp.pl>
2016-12-27 23:57:44 +0100

    Wipe out Options constructor

--- Wangscape/Options.cpp
+++ Wangscape/Options.cpp
@@ -1,18 +1,5 @@
 #include "Options.h"
-#include <iostream>
-#include <fstream>
-#include <istream>
-#include <stdexcept>
-#include <boost/filesystem.hpp>
 
-Options::Options(std::string filename):
-    filename(filename)
+Options::Options(std::string /*filename*/)
 {
-    std::ifstream ifs(filename);
-    if (!ifs.good())
-    {
-        throw std::runtime_error("Could not read options file");
-    }
-    std::string str{std::istreambuf_iterator<char>(ifs),
-                    std::istreambuf_iterator<char>()};
 }
