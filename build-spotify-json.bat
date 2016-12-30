e51bed35e5ab211d8635f308e44474a7f4ceea55
Serin Delaunay <serin.delaunay@gmail.com>
2016-12-30 23:04:15 +0000

    Autobuild spotify-json and double-conversion
    Using Boost from NuGet and generating lots of warnings

---
+++ build-spotify-json.bat
@@ -0,0 +1,11 @@
+cd lib\spotify-json
+REM cd vendor\double-conversion
+REM cmake .
+REM cmake --build . --config Debug
+REM cmake --build . --config Release
+REM cd ..\..
+cmake . -DBoost_NO_BOOST_CMAKE=TRUE ^
+        -DBOOST_ROOT=..\..\..\packages\boost.1.62.0.0\lib\native
+cmake --build . --config Debug
+cmake --build . --config Release
+cd ..\..
