72ffa66e3b4cfc859a337b2fd3a5917d7810898d
Serin Delaunay <serin.delaunay@gmail.com>
2016-12-31 06:05:13 +0000

    Make a batch file to cmake spotify-json

--- build-spotify-json.bat
+++ build-spotify-json.bat
@@ -1,11 +1,9 @@
 cd lib\spotify-json
-REM cd vendor\double-conversion
-REM cmake .
-REM cmake --build . --config Debug
-REM cmake --build . --config Release
-REM cd ..\..
-cmake . -DBoost_NO_BOOST_CMAKE=TRUE ^
-        -DBOOST_ROOT=..\..\..\packages\boost.1.62.0.0\lib\native
+rmdir build32 /S /Q
+mkdir build32
+cd build32
+cmake .. -DBoost_NO_BOOST_CMAKE=TRUE ^
+         -DBOOST_ROOT=..\..\..\packages\boost.1.62.0.0\lib\native
 cmake --build . --config Debug
 cmake --build . --config Release
-cd ..\..
+cd ..\..\..
