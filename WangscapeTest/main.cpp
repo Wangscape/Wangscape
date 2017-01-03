0d4830d31536ab6c1ebec1c1f97487b043264aab
Serin Delaunay <serin.delaunay@gmail.com>
2017-01-03 08:08:50 +0000

    Get the options file from command line args

--- WangscapeTest/main.cpp
+++ WangscapeTest/main.cpp
@@ -4,6 +4,12 @@
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
-    assert(TestRequiringOptions::initFilename("Wangscape/example/example_options.json"));
+    std::string filename;
+    if (argc < 2)
+        filename = std::string("Wangscape/example/example_options.json");
+    else
+        filename = std::string(argv[1]);
+    std::cout << "Using options file at " << filename << "\n";
+    assert(TestRequiringOptions::initFilename(filename));
     return RUN_ALL_TESTS();
 }
