76d5e20e965d120bb0f46b2dc13bcc70b532227d
Serin Delaunay <serin.delaunay@gmail.com>
2017-01-03 02:29:53 +0000

    Add batch file to autobuild spotify-json
    And leave the .lib files in a directory that agrees with
    spotify-json.props

---
+++ build-spotify-json.bat
@@ -0,0 +1,10 @@
+cd lib\spotify-json
+git submodule update --init
+rmdir build32 /S /Q
+mkdir build32
+cd build32
+cmake .. -DBoost_NO_BOOST_CMAKE=TRUE ^
+         -DBOOST_ROOT=..\..\..\packages\boost.1.62.0.0\lib\native
+cmake --build . --config Debug
+cmake --build . --config Release
+cd ..\..\..
