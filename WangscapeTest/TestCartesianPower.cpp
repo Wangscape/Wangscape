e1289254cee624de12d34aedc9278954c297d7b5
Serin Delaunay <serin.delaunay@gmail.com>
2017-01-07 22:10:10 +0000

    Add missing tests and divide them up

--- WangscapeTest/TestCartesianPower.cpp
+++ WangscapeTest/TestCartesianPower.cpp
@@ -7,6 +7,11 @@
 class TestCartesianPower : public ::testing::Test
 {
 protected:
+    typedef std::vector<int> Container;
+    typedef std::vector<int>::const_iterator Iterator;
+
+    Container base{4, 5, 6};
+    const tilegen::CartesianPower<Iterator> cp{base, 4};
     TestCartesianPower()
     {
 
@@ -14,42 +19,99 @@ protected:
     ~TestCartesianPower() {};
 };
 
-TEST_F(TestCartesianPower, TestCartesianPowerValues)
+TEST_F(TestCartesianPower, TestCartesianPowerSize)
 {
-    std::vector<int> base{4, 5, 6};
-    const tilegen::CartesianPower<std::vector<int>::const_iterator> cp(base, 4);
     EXPECT_EQ(81, cp.size()) <<
         "CartesianProduct has incorrect size";
-    auto cpit = cp.cbegin();
-    for (size_t i = 0; i < 4; i++)
-    {
-        EXPECT_EQ(4, *cpit.get(i)) <<
-            "CartesianProduct.cbegin() has incorrect value";
-    }
-    EXPECT_NE(cp.cbegin(), cp.cend());
-    ++cpit;
-    EXPECT_EQ(5, *cpit.get(0));
-    EXPECT_EQ(4, *cpit.get(1));
-    EXPECT_EQ(4, *cpit.get(2));
-    EXPECT_EQ(4, *cpit.get(3));
-    ++cpit; ++cpit;
-    EXPECT_EQ(4, *cpit.get(0));
-    EXPECT_EQ(5, *cpit.get(1));
-    EXPECT_EQ(4, *cpit.get(2));
-    EXPECT_EQ(4, *cpit.get(3));
-    std::advance(cpit, 77);
-    EXPECT_EQ(6, *cpit.get(0));
-    EXPECT_EQ(6, *cpit.get(1));
-    EXPECT_EQ(6, *cpit.get(2));
-    EXPECT_EQ(6, *cpit.get(3));
-    ++cpit;
-    EXPECT_EQ(cpit, cp.cend());
+}
+
+TEST_F(TestCartesianPower, TestCartesianPowerCount)
+{
     size_t count = 0;
-    const auto& cpc(cp);
-    std::as_const(cp);
     for (const auto& it : cp)
-    {
         count++;
+    EXPECT_EQ(81, count);
+}
+
+TEST_F(TestCartesianPower, TestCartesianPowerComparisonBeginEnd)
+{
+    EXPECT_EQ(cp.cbegin(), cp.cbegin());
+    EXPECT_NE(cp.cend(), cp.cbegin());
+    EXPECT_NE(cp.cbegin(), cp.cend());
+    EXPECT_EQ(cp.cend(), cp.cend());
+
+    EXPECT_EQ(cp.cbegin(), cp.begin());
+    EXPECT_EQ(cp.cend(), cp.end());
+}
+
+TEST_F(TestCartesianPower, TestCartesianPowerComparisonLast)
+{
+    auto it = cp.cbegin();
+    for (size_t i = 0; i < cp.size(); i++)
+    {
+        ++it;
     }
-    EXPECT_EQ(count, cp.size());
+    EXPECT_EQ(it, it);
+    EXPECT_EQ(it, cp.cend());
+    EXPECT_EQ(cp.cend(), it);
+    EXPECT_NE(it, cp.cbegin());
+    EXPECT_NE(cp.cbegin(), it);
+}
+
+TEST_F(TestCartesianPower, TestCartesianPowerComparisonOther)
+{
+    tilegen::CartesianPower<Iterator> cp1{base.cbegin() + 1, base.cend(), 4};
+    tilegen::CartesianPower<Iterator> cp2{base.cbegin(), base.cend(), 3};
+    EXPECT_NE(cp.begin(), cp1.begin());
+    EXPECT_NE(cp.begin(), cp2.begin());
+}
+
+TEST_F(TestCartesianPower, TestCartesianPowerComparisonOtherMid)
+{
+    auto it = cp.cbegin();
+    std::advance(it, 23);
+    EXPECT_EQ(it, it);
+    EXPECT_NE(it, cp.cbegin());
+    EXPECT_NE(cp.cbegin(), it);
+    EXPECT_NE(it, cp.cend());
+    EXPECT_NE(cp.cend(), it);
+}
+
+TEST_F(TestCartesianPower, TestCartesianPowerValueBegin)
+{
+    auto it = cp.cbegin();
+    EXPECT_EQ(4, *it.get(0));
+    EXPECT_EQ(4, *it.get(1));
+    EXPECT_EQ(4, *it.get(2));
+    EXPECT_EQ(4, *it.get(3));
+}
+
+TEST_F(TestCartesianPower, TestCartesianPowerValueBegin1)
+{
+    auto it = cp.cbegin();
+    ++it;
+    EXPECT_EQ(5, *it.get(0));
+    EXPECT_EQ(4, *it.get(1));
+    EXPECT_EQ(4, *it.get(2));
+    EXPECT_EQ(4, *it.get(3));
+}
+
+TEST_F(TestCartesianPower, TestCartesianPowerValueBegin01)
+{
+    auto it = cp.cbegin();
+    std::advance(it, 3);
+    EXPECT_EQ(4, *it.get(0));
+    EXPECT_EQ(5, *it.get(1));
+    EXPECT_EQ(4, *it.get(2));
+    EXPECT_EQ(4, *it.get(3));
+}
+
+TEST_F(TestCartesianPower, TestCartesianPowerValueLast)
+{
+    auto it = cp.cbegin();
+    std::advance(it, 80);
+    EXPECT_EQ(6, *it.get(0));
+    EXPECT_EQ(6, *it.get(1));
+    EXPECT_EQ(6, *it.get(2));
+    EXPECT_EQ(6, *it.get(3));
 }
