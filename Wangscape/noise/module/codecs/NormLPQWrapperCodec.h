67d68d39d4d359866be83790bfc12ddde4b4a7c0
Serin Delaunay <serin.delaunay@gmail.com>
2017-02-04 02:54:19 +0000

    Codec for Wrapper<NormLPQ>

---
+++ Wangscape/noise/module/codecs/NormLPQWrapperCodec.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <spotify/json.hpp>
+
+#include <noise/noise.h>
+#include "noise/module/Wrapper.h"
+#include "noise/module/NormLPQ.h"
+
+namespace spotify
+{
+namespace json
+{
+
+template<>
+struct default_codec_t<noise::module::Wrapper<noise::module::NormLPQ>>
+{
+    using NormLPQWrapper = noise::module::Wrapper<noise::module::NormLPQ>;
+    static codec::object_t<NormLPQWrapper> codec()
+    {
+        auto codec = codec::object<NormLPQWrapper>();
+        codec.required("type", codec::eq<std::string>("NormLPQ"));
+        codec.optional("P",
+                       [](const NormLPQWrapper& mw) {return mw.module.GetP(); },
+                       [](NormLPQWrapper& mw, double p) {mw.module.SetP(p); });
+        codec.optional("Q",
+                       [](const NormLPQWrapper& mw) {return mw.module.GetQ(); },
+                       [](NormLPQWrapper& mw, double q) {mw.module.SetQ(q); });
+        return codec;
+    }
+};
+}
+}
