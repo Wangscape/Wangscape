eb416633664be02486ce9a3d9775751972bd7fc6
Serin Delaunay <serin.delaunay@gmail.com>
2017-02-04 02:53:25 +0000

    codec for Wrapper<CornerCombinerBase>

---
+++ Wangscape/noise/module/codecs/CornerCombinerBaseWrapperCodec.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <spotify/json.hpp>
+
+#include <noise/noise.h>
+#include "noise/module/Wrapper.h"
+#include "noise/module/CornerCombinerBase.h"
+
+namespace spotify
+{
+namespace json
+{
+
+template<>
+struct default_codec_t<noise::module::Wrapper<noise::module::CornerCombinerBase>>
+{
+    using CornerCombinerBaseWrapper = noise::module::Wrapper<noise::module::CornerCombinerBase>;
+    static codec::object_t<CornerCombinerBaseWrapper> codec()
+    {
+        auto codec = codec::object<CornerCombinerBaseWrapper>();
+        codec.required("type", codec::eq<std::string>("CornerCombinerBase"));
+        codec.optional("Power",
+                       [](CornerCombinerBase CornerCombinerBaseWrapper& mw) {return mw.module.GetPower(); },
+                       [](CornerCombinerBaseWrapper& mw, double power) {mw.module.SetPower(power); });
+        return codec;
+    }
+};
+}
+}
