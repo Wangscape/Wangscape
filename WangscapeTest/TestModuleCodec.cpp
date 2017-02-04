d02a06cae2ab82dce44df046607da63fb283470b
Serin Delaunay <serin.delaunay@gmail.com>
2017-02-04 15:20:30 +0000

    Use formatted JSON in module codec tests
    spotify-json does not include whitespace in output JSON, so whitespace
    must be removed when comparing canonical JSON to encoding output.

--- WangscapeTest/TestModuleCodec.cpp
+++ WangscapeTest/TestModuleCodec.cpp
@@ -1,49 +1,91 @@
 #include <gtest/gtest.h>
 
 #include <memory>
+#include <algorithm>
+#include <string>
 
-#include <noise/module/codecs/PerlinWrapperCodec.h>
-#include <noise/module/codecs/VoronoiWrapperCodec.h>
-#include <noise/module/codecs/NoiseSourcesCodec.h>
+#include <noise/module/codecs/ModuleCodecs.h>
 
 class TestModuleCodec : public ::testing::Test {
 protected:
+    void remove_whitespace(std::string& s)
+    {
+        s.erase(std::remove_if(s.begin(),
+                               s.end(),
+                               [](char c) { return std::isspace(c, std::locale()); }),
+                s.end());
+    }
 };
 
 TEST_F(TestModuleCodec, TestPerlinEncode)
 {
     auto p = std::make_shared<noise::module::Wrapper<noise::module::Perlin>>();
     std::string s(spotify::json::encode(p));
-    ASSERT_EQ(s, "{\"type\":\"Perlin\",\"Frequency\":1,\"Lacunarity\":2,"
-              "\"NoiseQuality\":\"Standard\",\"OctaveCount\":6,\"Persistence\":0.5,\"Seed\":0}");
+    std::string expected(R"(
+{
+    "type" : "Perlin",
+    "Frequency" : 1,
+    "Lacunarity" : 2,
+    "NoiseQuality" : "Standard",
+    "OctaveCount" : 6,
+    "Persistence" : 0.5,
+    "Seed" : 0
+}
+)");
+    remove_whitespace(expected);
+    ASSERT_EQ(expected, s);
+}
+
+TEST_F(TestModuleCodec, TestPerlinDecode)
+{
+    std::string s(R"(
+{
+    "type" : "Perlin",
+    "Frequency" : 1.7,
+    "Lacunarity" : 0.999,
+    "NoiseQuality" : "Fast",
+    "OctaveCount" : 8,
+    "Persistence" : 0.001,
+    "Seed" : 101
+}
+)");
+    auto p = spotify::json::decode<std::shared_ptr<noise::module::Wrapper<noise::module::Perlin>>>(s);
+    EXPECT_EQ(1.7, p->module.GetFrequency());
+    EXPECT_EQ(0.999, p->module.GetLacunarity());
+    EXPECT_EQ(noise::QUALITY_FAST, p->module.GetNoiseQuality());
+    EXPECT_EQ(8, p->module.GetOctaveCount());
+    EXPECT_EQ(0.001, p->module.GetPersistence());
+    EXPECT_EQ(101, p->module.GetSeed());
 }
 
 TEST_F(TestModuleCodec, TestVoronoiEncode)
 {
     auto v = std::make_shared<noise::module::Wrapper<noise::module::Voronoi>>();
     std::string s(spotify::json::encode(v));
-    ASSERT_EQ(s, "{\"type\":\"Voronoi\",\"Frequency\":1,"
-              "\"Displacement\":1,\"DistanceEnabled\":false,\"Seed\":0}");
-}
-
-TEST_F(TestModuleCodec, TestPerlinDecode)
+    std::string expected(R"(
 {
-    std::string s("{\"type\":\"Perlin\", \"Frequency\" : 1.7, \"Lacunarity\" : 0.999, "
-                  "\"NoiseQuality\" : \"Fast\", \"OctaveCount\" : 8, "
-                  "\"Persistence\" : 0.001, \"Seed\" : 101}");
-    auto p = spotify::json::decode<std::shared_ptr<noise::module::Wrapper<noise::module::Perlin>>>(s);
-    EXPECT_EQ(1.7, p->module.GetFrequency()) << s;
-    EXPECT_EQ(0.999, p->module.GetLacunarity()) << s;
-    EXPECT_EQ(noise::QUALITY_FAST, p->module.GetNoiseQuality()) << s;
-    EXPECT_EQ(8, p->module.GetOctaveCount()) << s;
-    EXPECT_EQ(0.001, p->module.GetPersistence()) << s;
-    EXPECT_EQ(101, p->module.GetSeed()) << s;
+    "type" : "Voronoi",
+    "Frequency" : 1,
+    "Displacement" : 1,
+    "DistanceEnabled" : false,
+    "Seed" : 0
+}
+)");
+    remove_whitespace(expected);
+    ASSERT_EQ(expected, s);
 }
 
 TEST_F(TestModuleCodec, TestVoronoiDecode)
 {
-    std::string s("{\"type\":\"Voronoi\", \"Frequency\" : 2.7, \"Displacement\" : 3.1, "
-                  "\"DistanceEnabled\" : true, \"Seed\" : 47}");
+    std::string s(R"(
+{
+    "type" : "Voronoi" ,
+    "Frequency" : 2.7,
+    "Displacement" : 3.1,
+    "DistanceEnabled" : true,
+    "Seed" : 47
+}
+)");
     auto v = spotify::json::decode<std::shared_ptr<noise::module::Wrapper<noise::module::Voronoi>>>(s);
     EXPECT_EQ(2.7, v->module.GetFrequency()) << s;
     EXPECT_EQ(3.1, v->module.GetDisplacement()) << s;
