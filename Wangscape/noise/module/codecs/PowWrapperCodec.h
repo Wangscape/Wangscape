fcecef3c0ae77593c7bfb01fd8b83b0bb552c1bf
Serin Delaunay <serin.delaunay@gmail.com>
2017-02-04 02:53:53 +0000

    Codec for Wrapper<Pow>

---
+++ Wangscape/noise/module/codecs/PowWrapperCodec.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <spotify/json.hpp>
+
+#include <noise/noise.h>
+#include "noise/module/Wrapper.h"
+#include "noise/module/Pow.h"
+
+namespace spotify
+{
+namespace json
+{
+
+template<>
+struct default_codec_t<noise::module::Wrapper<noise::module::Pow>>
+{
+    using PowWrapper = noise::module::Wrapper<noise::module::Pow>;
+    static codec::object_t<PowWrapper> codec()
+    {
+        auto codec = codec::object<PowWrapper>();
+        codec.required("type", codec::eq<std::string>("Pow"));
+        codec.required("SourceModule", codec::ignore_t<int>());
+        codec.optional("Exponent",
+                       [](const PowWrapper& mw) {return mw.module.GetExponent(); },
+                       [](PowWrapper& mw, double exponent) {mw.module.SetExponent(exponent); });
+        return codec;
+    }
+};
+}
+}
