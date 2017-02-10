05375b10cfd6e060242c9786fb7887dcd3850ebc
Serin Delaunay <serin.delaunay@gmail.com>
2017-02-10 01:41:41 +0000

    Allow specification of NoiseQuality with an int

--- Wangscape/noise/module/codecs/NoiseQualityCodec.h
+++ Wangscape/noise/module/codecs/NoiseQualityCodec.h
@@ -13,15 +13,21 @@ template<>
 struct default_codec_t<noise::NoiseQuality>
 {
     using NoiseQuality = noise::NoiseQuality;
-    static codec::enumeration_t<NoiseQuality, codec::string_t> codec()
+    static codec::one_of_t<
+        codec::enumeration_t<NoiseQuality, codec::number_t<int>>,
+        codec::enumeration_t<NoiseQuality, codec::string_t>> codec()
     {
-        auto codec = codec::enumeration<NoiseQuality, std::string>({
+        auto codec_str = codec::enumeration<NoiseQuality, std::string>({
             {NoiseQuality::QUALITY_FAST, "Fast"},
             {NoiseQuality::QUALITY_STD, "Standard"},
             {NoiseQuality::QUALITY_BEST, "Best"}
         });
-
-        return codec;
+        auto codec_int = codec::enumeration<NoiseQuality, int>({
+            {NoiseQuality::QUALITY_FAST, 0},
+            {NoiseQuality::QUALITY_STD, 1},
+            {NoiseQuality::QUALITY_BEST, 2}
+        });
+        return codec::one_of(codec_int, codec_str);
     }
 };
 
