8e6c6d0291cdeae0a79b007a802f72f17a89134b
Serin Delaunay <serin.delaunay@gmail.com>
2017-02-13 07:05:14 +0000

    Use std::transform in Curve codec

--- Wangscape/noise/module/codecs/CurveWrapperCodec.cpp
+++ Wangscape/noise/module/codecs/CurveWrapperCodec.cpp
@@ -20,14 +20,16 @@ codec::object_t<CurveWrapper> default_codec_t<CurveWrapper>::codec()
                            std::vector<std::pair<double, double>> all_control_points;
                            const noise::module::ControlPoint* raw_control_points = mw.module->GetControlPointArray();
                            const int control_point_count = mw.module->GetControlPointCount();
-                           for (int i = 0; i < control_point_count; i++)
+                           std::transform(raw_control_points,
+                                          raw_control_points + control_point_count,
+                                          std::inserter(all_control_points, all_control_points.begin()),
+                                          [](noise::module::ControlPoint control_point)
                            {
-                               const noise::module::ControlPoint control_point = raw_control_points[i];
-                               all_control_points.push_back({control_point.inputValue, control_point.outputValue});
-                           }
+                               return std::make_pair(control_point.inputValue, control_point.outputValue);
+                           });
                            return all_control_points;
                        },
-                                      [](CurveWrapper& mw, std::vector<std::pair<double, double>> all_control_points)
+                       [](CurveWrapper& mw, std::vector<std::pair<double, double>> all_control_points)
                        {
                            for (std::pair<double, double> control_point : all_control_points)
                            {
