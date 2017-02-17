3fc59ffef656a290afcb8f02c349e747c65f1cd8
Serin Delaunay <serin.delaunay@gmail.com>
2017-02-17 07:27:15 +0000

    Use RESTnoise module group format; correct errors

--- Wangscape/noise/codecs/EncodedModuleGroupCodec.h
+++ Wangscape/noise/codecs/EncodedModuleGroupCodec.h
@@ -15,11 +15,11 @@ struct default_codec_t<noise::EncodedModuleGroup>
     static codec::object_t<noise::EncodedModuleGroup> codec()
     {
         auto codec = codec::object<noise::EncodedModuleGroup>();
-        codec.required("Modules", &noise::EncodedModuleGroup::encodedModules);
-        codec.optional("InputModules",
+        codec.required("modules", &noise::EncodedModuleGroup::encodedModules);
+        codec.optional("inputmodules",
                        [](const noise::EncodedModuleGroup& emg) { return emg.moduleGroup->mInputModules; },
                        [](noise::EncodedModuleGroup& emg,std::vector<noise::ModuleID> input_modules){ emg.moduleGroup->mInputModules = input_modules; });
-        codec.optional("OutputModule",
+        codec.optional("defaultoutputmodule",
                        [](const noise::EncodedModuleGroup& emg) { return emg.moduleGroup->mOutputModule; },
                        [](noise::EncodedModuleGroup& emg, noise::ModuleID output_module) { emg.moduleGroup->mOutputModule = output_module; });
 
