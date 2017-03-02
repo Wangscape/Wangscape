acdec984a3c9911a657331a2b915c0dc742b7dba
Serin Delaunay <serin.delaunay@gmail.com>
2017-03-02 10:58:26 +0000

    Use override specifier in Dither's updateAlphasAux

--- Wangscape/tilegen/alpha/CalculatorDither.h
+++ Wangscape/tilegen/alpha/CalculatorDither.h
@@ -13,7 +13,7 @@ class CalculatorDither : public CalculatorTopTwo
 public:
     CalculatorDither();
 protected:
-    virtual void updateAlphasAux(const Weights& weights);
+    virtual void updateAlphasAux(const Weights& weights) override;
 private:
     std::mt19937 mRNG;
     std::uniform_real_distribution<double> mUniformRealDistribution;
