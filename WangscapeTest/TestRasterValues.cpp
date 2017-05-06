624b519643036b31b714f88ccd02cc41c4c8ffe6
Serin Delaunay <serin.delaunay@gmail.com>
2017-05-06 20:29:23 +0100

    Empty test class for RasterValues

---
+++ WangscapeTest/TestRasterValues.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+
+#include <noise/RasterValues.h>
+
+class TestRasterValues : public ::testing::Test
+{
+protected:
+    TestRasterValues()
+    {
+
+    };
+    ~TestRasterValues() {};
+};
+
+TEST_F(TestRasterValues, TestRasterValuesBuild)
+{
+
+}
