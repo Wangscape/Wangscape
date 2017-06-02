7ebab12826fa278df3aa5b9155d3fc8266ff06d3
Serin Delaunay <serin.delaunay@gmail.com>
2017-06-02 15:41:57 +0100

    Delete travis-ubuntu-docker.sh

--- travis-ubuntu-docker.sh
+++
@@ -1,4 +0,0 @@
-cd /Wangscape/build && cmake .. && make
-Xvfb :1 -screen 0 1600x900x24 &> xvfb.log & DISPLAY=:1.0
-export DISPLAY
-xvfb-run --server-args "-screen 0 1600x900x24" ./bin/WangscapeTest ../doc
