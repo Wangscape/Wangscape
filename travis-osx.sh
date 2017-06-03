97a4c33468f123b723980c8b6669de95f2fefdfe
Serin Delaunay <serin.delaunay@gmail.com>
2017-06-04 00:19:05 +0100

    Travis OSX: Remove googletest build steps

--- travis-osx.sh
+++ travis-osx.sh
@@ -2,14 +2,6 @@ pwd
 git submodule update --init --recursive
 brew install sfml
 
-git clone https://github.com/google/googletest.git
-cd googletest
-mkdir build
-cd build
-cmake ..
-make
-cd ../..
-
 mkdir build
 cd build
 cmake .. -DCMAKE_MODULE_PATH=/usr/local/opt/sfml/share/SFML/cmake/Modules
