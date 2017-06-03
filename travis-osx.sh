c28a01d282358e40a69dbcfca4e94b35c1258ee4
Serin Delaunay <serin.delaunay@gmail.com>
2017-06-03 03:41:32 +0100

    Build googletest and see if CMake can find it

--- travis-osx.sh
+++ travis-osx.sh
@@ -1,6 +1,16 @@
 pwd
 git submodule update --init --recursive
 brew install sfml
+
+git clone https://github.com/google/googletest.git
+cd googletest
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
+cd ../..
+
 mkdir build
 cd build
 cmake .. -DCMAKE_MODULE_PATH=/usr/local/opt/sfml/share/SFML/cmake/Modules
