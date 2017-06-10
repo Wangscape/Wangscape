f563ac3a8ebf8e2c9ef80d53db216d68cc2a5cbf
Łukasz Hryniuk <lukasz.hryniuk@wp.pl>
2017-06-11 01:38:24 +0200

    Adjust logging implementation to Logger class

--- Wangscape/logging/Logging.h
+++ Wangscape/logging/Logging.h
@@ -1,21 +1,21 @@
 #pragma once
 
-#include "AppenderType.h"
 #include "Level.h"
 #include "Logger.h"
 
 #include <ostream>
 
-logging::Logger& logDebug();
-logging::Logger& logInfo();
-logging::Logger& logWarning();
-logging::Logger& logError();
+logging::Logger<logging::Level::Debug> logDebug();
+logging::Logger<logging::Level::Info> logInfo();
+logging::Logger<logging::Level::Warning> logWarning();
+logging::Logger<logging::Level::Error> logError();
 
 namespace logging
 {
 
-void setLoggingLevel(Level level);
-void setAppender(AppenderType appenderType, std::string name="");
-Logger& getLogger(Level level);
+void addAppender(std::unique_ptr<Appender> appender);
+void setLevel(std::string appender_name, logging::Level new_level);
+template<typename LevelEnum, LevelEnum Level>
+Logger<Level> log();
 
 } // namespace logging
